8b0b06d3ca5f98cf0b1f54321971ce7b5f85e08b
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-10-17 10:19:07 -0400

    teste_git

---
+++ Aulas/GitBash.py
@@ -0,0 +1,5 @@
+# cd Pasta/Selecionada
+# git pull
+# git add .
+# git commit -m "nome"
+# git push
--- Aulas/Poo/main.py
+++ Aulas/Poo/main.py
@@ -5,8 +5,8 @@ class Carro:
         self.cor = cor
         self.ano = ano
 
-fusca = Carro('VolksWagen','Fusca','Preto',1967)
-civic = Carro('Honda',"Civic","Vermelho",1945)
+    def acelerar(self):
+        print(f'O carro {self.modelo} da marca {self.marca} acelerou. ')
 
 class Moto:
     def __init__(self, marca, modelo, cor, ano):
@@ -14,16 +14,20 @@ class Moto:
         self.modelo = modelo
         self.cor = cor
         self.ano = ano
+    
+    def acelerar(self):
+        print(f'A moto {self.modelo} da marca {self.marca} acelerou. ')
 
+fusca = Carro('VolksWagen','Fusca','Preto',1967)
+civic = Carro('Honda',"Civic","Vermelho",1945)
 harley = Moto('Harley Davidson','FatBoy','Azul',2019)
 
+
+print("\n======== Carros ========")
 print(vars(fusca))
 print(vars(civic))
-print("")
 
+
+print("\n======== Motos ========")
 for key, value in vars(harley).items():
     print(f"{key}: {value}")
-
-
-
-
