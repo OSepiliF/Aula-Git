2453240afa496fc2c1c2da708c33ba529b1c8caf
Filipe Oliveira Simões <filipeosimoes@gmail.com>
2024-10-17 09:21:01 -0400

    testezinho

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ class Carro:
         self.cor = cor
         self.ano = ano
 
-fusca = Carro('VolksWagen','Fusca','Preto','1967')
+fusca = Carro('VolksWagen','Fusca','Preto',1967)
+civic = Carro('Honda',"Civic","Vermelho",1945)
 
-print(fusca.ano)
+class Moto:
+    def __init__(self, marca, modelo, cor, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+
+harley = Moto('Harley Davidson','FatBoy','Azul',2019)
+
+print(vars(fusca))
+print(vars(civic))
+print("")
+
+for key, value in vars(harley).items():
+    print(f"{key}: {value}")
+
+
+
+
